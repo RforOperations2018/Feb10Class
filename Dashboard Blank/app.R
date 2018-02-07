bde0fb9358112d153d75ecdf6f61eac1e99d100f
geoffreylarnold <geoffrey.lloyd.arnold@gmail.com>
2018-02-07 11:56:56 -0500

    blank fix

--- Dashboard Blank/app.R
+++ Dashboard Blank/app.R
@@ -38,29 +38,7 @@ ui <- dashboardPage(header, sidebar, body)
 
 # Define server logic
 server <- function(input, output) {
-  output$plot_mass <- renderPlotly({
-    dat <- subset(meltwars, name %in% input$char_select & variable == "mass")
-    ggplot(data = dat, aes(x = name, y = as.numeric(value), fill = name)) + geom_bar(stat = "identity")
-  })
-  output$plot_height <- renderPlotly({
-    dat <- subset(meltwars, name %in% input$char_select & variable == "height")
-    ggplot(data = dat, aes(x = name, y = as.numeric(value), fill = name)) + geom_bar(stat = "identity")
-  })
-  output$table <- DT::renderDataTable({
-    subset(starwars, name %in% input$char_select, select = c(name, height, mass, birth_year, homeworld, species))
-  })
-  output$mass <- renderInfoBox({
-    sw <- subset(starwars, name %in% input$char_select)
-    num <- round(mean(sw$mass, na.rm = T), 2)
-    
-    infoBox("Avg Mass", value = num, subtitle = paste(nrow(sw), "characters"), icon = icon("balance-scale"), color = "purple")
-  })
-  output$height <- renderValueBox({
-    sw <- subset(starwars, name %in% input$char_select)
-    num <- round(mean(sw$height, na.rm = T), 2)
-    
-    valueBox(subtitle = "Avg Height", value = num, icon = icon("sort-numeric-asc"), color = "green")
-  })
+
 }
 
 # Run the application 
